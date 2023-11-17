831bf1c4a32ae437dbb462b53bda74e1d28ebd38
Pelumi Adeyanju <pelumiadeyanju39@gmail.com>
2023-11-17 01:53:11 -0800

    second commit

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const Reg = /[\W_]/g;
 const paragraph = document.querySelector('.js-para');
 const checkButton = document.querySelector('.js-button');
 
-function palidrome() {
+function palindrome() {
   const str = inputElement.value;
   const smallStr = str.toLowerCase().replace(Reg, '');
   const reversed = smallStr.split('').reverse().join('');
 
   if(reversed === smallStr) {
-    paragraph.innerHTML = `${true}, It is a palidrome.`;
+    paragraph.innerHTML = `${true}, It is a palindrome.`;
   }else {
-    paragraph.innerHTML = `${false}, It is not Palidrome.`;
+    paragraph.innerHTML = `${false}, It is not a Palindrome.`;
   }
 
   inputElement.value = '';
 }
 
-checkButton.addEventListener('click', palidrome);
+checkButton.addEventListener('click', palindrome);
